742088d63e9b7953a255bfe63bf70903b05ae81c
Shigeki Ohtsu <ohtsu@ohtsu.org>
2021-03-03 15:29:28 +0900

    add description

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
 const router = require('./lib/router');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.text({defaultCharset: 'utf-8'}));
 app.use('/ask_practice', router);
 app.use('/', router);
 app.listen(9876, '127.0.0.1', () => {
--- server/lib/router.js
+++ server/lib/router.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
-const handler = (req, res) => {
+const getHandler = (req, res) => {
   if (!req.query.name) {
     res.send(`利用方法が違うようです。<a href="https://github.com/shigeki/ask_practice/tree/WIP/manual">https://github.com/shigeki/ask_practice/tree/WIP/manual</a> を参照の上、再度アクセスしてください。`);
     return;
   }
   const name = req.query.name;
-  res.send(`${name}様: 申し訳ございません。システムに問題が発生しましたので <a href="https://github.com/shigeki/ask_practice/issues">https://github.com/shigeki/ask_practice/issues</a> までお問い合わせください。`);
+  res.header('Content-Type', 'text/html;charset=utf-8');
+  res.send(`${name}様: 申し訳ございません。システムに問題が発生しましたので <a href="https://github.com/shigeki/ask_practice/issues">https://github.com/shigeki/ask_practice/issues</a> までお問い合わせください。github使いたくない、もしくはアカウントのない方は <a href="https://html5.ohtsu.org/question.html">https://html5.ohtsu.org/question.html</a> までお問い合わせください`);
 };
-router.get('/ask_practice', (req, res) => handler(req, res));
-router.get('/', (req, res) => handler(req, res));
+const postHandler = (req, res) => {
+  const message = req.body.message;
+  console.log('message:', message);
+  res.header('Content-Type', 'text/html;charset=utf-8');
+  res.send('問い合わせありがとうございました。');
+};
+router.get('/ask_practice', (req, res) => getHandler(req, res));
+router.get('/', (req, res) => getHandler(req, res));
+router.post('/ask_practice', (req, res) => postHandler(req, res));
+router.post('/', (req, res) => postHandler(req, res));
+
 module.exports = router;
